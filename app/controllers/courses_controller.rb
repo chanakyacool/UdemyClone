d93943f4871dc0cb88490ddf482066619b7a3e38
Selim Reza <selimrezael@gmail.com>
2016-07-31 14:00:09 +0600

    course creation workable

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,28 @@
+class CoursesController < ApplicationController
+
+  def index
+    @courses = Course.all
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    @course.save
+    redirect_to @course
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description)
+  end
+
+end
