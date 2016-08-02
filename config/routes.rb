6d940ecd9f0bcd2c212524c4ccf01538610dc09a
Selim Reza <selimrezael@gmail.com>
2016-08-02 14:05:33 +0600

    auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   resources :courses
 
-  root 'welcome#index'
+  authenticated do
+    root 'courses#index'
+  end
+
+  unauthenticated do
+    root 'welcome#index'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
