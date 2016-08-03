3fb4ffb076fff739533738d1b2a67a1660d8cceb
Selim Reza <selimrezael@gmail.com>
2016-08-03 22:44:31 +0600

    removed sections show

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,9 +1,9 @@
 class SectionsController < ApplicationController
 
-  def show
-   @course = Course.find(params[:course_id])
-   @section = @course.sections.find(params[:id])
-  end
+  #def show
+   #@course = Course.find(params[:course_id])
+   #@section = @course.sections.find(params[:id])
+  #end
 
   def new
     @course = Course.find(params[:course_id])
