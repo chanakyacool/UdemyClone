ec8d7b1d0efa21d1a82a764c5ba884e6e9b9c9a7
Selim Reza <selimrezael@gmail.com>
2016-08-08 12:36:58 +0600

    edit profile not yet working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit({ roles: [] }, :full_name, :email, :username, :password,
-        :password_confirmation, :title, :bio, :avatar, :fb_link, :tw_link,
-        :li_link, :yt_link)
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :email, :password, :current_password, :title, :bio, :avatar, :fb_link, :tw_link, :li_link, :yt_link) }
     end
-  end
 end
